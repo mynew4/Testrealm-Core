f0badf6c47e9dbc73bedcd4316ed8e39ba864599
Exitare <raphael.kirchgaessner@gmail.com>
2015-09-22 14:56:32 +0200

    [Custom] port

--- src/server/scripts/Custom/teleportitem.cpp
+++ src/server/scripts/Custom/teleportitem.cpp
@@ -37,7 +37,7 @@ class port_item : public ItemScript
 	public:
 		port_item() : ItemScript("teleportitem"){}
 
-		boolean Onuse(Player * player, Item *, SpellCastTargets const&)
+		bool Onuse(Player * player, Item *, SpellCastTargets const&)
 		{
 			player->TeleportTo(0, -9773.22, 2125.16, 15.49, 3.47, 0.0f);
 			return true;
