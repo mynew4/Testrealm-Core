6eb986de27be3f8436f31d1887e766b01b721a3e
Exitare <raphael.kirchgaessner@gmail.com>
2015-09-23 18:32:04 +0200

    [Custom] worldserver

--- src/server/worldserver/Main.cpp
+++ src/server/worldserver/Main.cpp
@@ -53,7 +53,7 @@
 using namespace boost::program_options;
 
 #ifndef _TRINITY_CORE_CONFIG
-    #define _TRINITY_CORE_CONFIG  "worldserver.conf"
+    #define _TRINITY_CORE_CONFIG  "worldserver2.conf"
 #endif
 
 #define WORLD_SLEEP_CONST 50
