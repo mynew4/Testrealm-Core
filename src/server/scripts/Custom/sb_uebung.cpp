38008081ae49a506ad302dff2bdc95e4a6b2c9da
Exitare <exi@pihaar.de>
2015-12-08 12:49:53 +0100

    [Custom] Add Sturmbringer Script

---
+++ src/server/scripts/Custom/sb_uebung.cpp
@@ -0,0 +1,43 @@
+#include "AccountMgr.h"
+#include "time.h"
+#include <stdio.h>
+#include "Bag.h"
+#include "Common.h"
+#include "Config.h"
+#include "DatabaseEnv.h"
+#include "DBCStructure.h"
+#include "Define.h"
+#include "Field.h"
+#include "GameEventMgr.h"
+#include "Item.h"
+#include "ItemPrototype.h"
+#include "Language.h"
+#include "Log.h"
+#include "ObjectGuid.h"
+#include "ObjectMgr.h"
+#include "Player.h"
+#include "QueryResult.h"
+#include "ScriptMgr.h"
+#include "SharedDefines.h"
+#include "Transaction.h"
+#include "WorldSession.h"
+#include <sstream>
+#include <string>
+#include <stdlib.h>
+
+
+class sb_uebung : public CreatureScript
+{
+
+public:
+	sb_uebung() : CreatureScript("sb_uebung") { }
+
+
+
+};
+
+
+void AddSC_sb_uebung()
+{
+	new sb_uebung();
+}
