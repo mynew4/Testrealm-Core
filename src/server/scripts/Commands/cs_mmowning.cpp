9241f9ec08901d447a7d3db20e8b1ae85c9c8f96
Exitare <exi@pihaar.de>
2015-12-09 22:07:34 +0100

    [Custom] werbung text neu eingespielt

--- src/server/scripts/Commands/cs_mmowning.cpp
+++ src/server/scripts/Commands/cs_mmowning.cpp
@@ -437,7 +437,7 @@ public:
 			std::ostringstream tt;
 
 
-			tt << "Spieler gefunden: " << eingabe << "\nEs sind: " << anzahl << " Einträge vorhanden. Uberpruefung ist sinnvoll.";
+			tt << "Spieler gefunden: " << eingabe << "Es sind: " << anzahl << " Einträge vorhanden. Uberpruefung ist sinnvoll.";
 			sWorld->SendGMText(LANG_GM_BROADCAST, tt.str().c_str());
 			
 			return true;
