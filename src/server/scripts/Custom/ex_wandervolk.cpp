a217d60988e780792aa7a7e4e78068fdc0f6ce02
Exitare <exi@pihaar.de>
2015-12-28 18:15:54 +0100

    [Custom] Test

--- src/server/scripts/Custom/ex_wandervolk.cpp
+++ src/server/scripts/Custom/ex_wandervolk.cpp
@@ -233,7 +233,7 @@ public: lucionnpc() : CreatureScript("lucion"){ }
 				Player* player = player->GetSession()->GetPlayer();
 				Quest const* quest;
 				quest = sObjectMgr->GetQuestTemplate(900811);
-				player->AddQuest(quest,nullptr);
+				//player->AddQuest(quest,nullptr);
 			}
 
 
@@ -266,7 +266,7 @@ public: lucionnpc() : CreatureScript("lucion"){ }
 
 		bool OnQuestAccept(Player* player, Creature* creature, Quest const* quest) { 
 			if (quest->GetQuestId() == 900825){
-				creature->HandleEmoteCommand(EMOTE_ONESHOT_LOOT);
+				creature->HandleEmoteCommand(EMOTE_ONESHOT_APPLAUD);
 				creature->Yell("Hoert mir zu. Ich muss euch etwas wichtiges erzaehlen bevor wir hier weitermachen koennen. Groot und Kraserius von den Sammlern verdaechtigen mich, das ich ein Verraeter sei und nicht im Interesse von uns handeln wuerde. Aber ich kann Euch versichern, dem ist nicht so. Es ist eher anders, die beiden betruegen uns und das gesamte Volk. Sie nutzen uns aus und berreichern sich selbst. Glaubt mir! Ich moechte nicht das auch ihr ausgenutzt werdet.", LANG_UNIVERSAL, NULL);
 			}
 			return true;
