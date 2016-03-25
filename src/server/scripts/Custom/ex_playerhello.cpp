43e1952f24bb34bfe112965c62665f04dbdea889
Exitare <exi@avarius.net>
2016-03-25 20:02:16 +0100

    [Custom] Eptest

--- src/server/scripts/Custom/ex_playerhello.cpp
+++ src/server/scripts/Custom/ex_playerhello.cpp
@@ -531,11 +531,11 @@ public:
     }
     
     
-	void OnGiveXP(Player* player, uint32& amount, Unit* victim)
+	void OnGiveXP(Player* player, uint32& amount, Unit* /*victim*/)
 	{
         player->GetSession()->SendNotification("Du hast 400 Bonus-ep erhalten");
-        player->GiveXP(amount,nullptr);
-        return true;
+        amount = amount *2;
+        return;
         
       /*  boost::gregorian::date date(boost::gregorian::day_clock::local_day());
         
