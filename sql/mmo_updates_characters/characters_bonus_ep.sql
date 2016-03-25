392ac1573967152a0f823bc7bafd0d1952934ce9
Exitare <exi@avarius.net>
2016-03-25 12:57:27 +0100

    [SQL] new db

---
+++ sql/mmo_updates_characters/characters_bonus_ep.sql
@@ -0,0 +1,7 @@
+CREATE TABLE `bonus_ep` (
+  `id` INT NOT NULL AUTO_INCREMENT COMMENT '',
+  `player` VARCHAR(45) NOT NULL COMMENT '',
+  `playerid` INT NOT NULL COMMENT '',
+  `start` INT NOT NULL COMMENT '',
+  `ende` INT NOT NULL COMMENT '',
+  PRIMARY KEY (`id`)  COMMENT '');
